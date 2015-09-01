a725134797d39250f7c0336155c87ed87434431b
andre-dietrich <dietrich@ivs.cs.uni-magdeburg.de>
2015-09-01 11:26:30 +0200

    config files and temporary files are now part of folder data

--- scan.sh
+++ scan.sh
@@ -65,10 +65,9 @@ parse_config()
   echo "------------------------------------------------------------"
   if [ -z "$OSM_MODE" ]
   then
-    echo "osm mode: DEFAULT"
-  else
-    echo "osm mode: $OSM_MODE"
+    OSM_MODE="DEFAULT"
   fi
+  echo "osm mode: $OSM_MODE"
   echo "------------------------------------------------------------"
   echo "bing-key: $BING_KEY"
   echo "------------------------------------------------------------"
@@ -86,7 +85,7 @@ parse_config()
 
 parse_meta()
 {
-  METAFILE="./data/$HASH_ID.meta"
+  METAFILE="./data/tmp/$HASH_ID.meta"
 
   parse $METAFILE
 
@@ -103,13 +102,13 @@ grab_osm()
   echo "============================================================"
   echo "OSM-GRAB"
   echo "============================================================"
-  if [ -a "./data/$HASH_ID.osm" ]
+  if [ -a "./data/tmp/$HASH_ID.osm" ]
   then
     echo "skipping, file already exists ..."
-    echo "./data/$HASH_ID.osm"
+    echo "./data/tmp/$HASH_ID.osm"
   else
-    echo "wget -N -c -O ./data/$HASH_ID.osm http://overpass-api.de/api/map?bbox=$GPS_FROM1,$GPS_FROM2,$GPS_TO1,$GPS_TO2"
-    wget -N -c -O ./data/$HASH_ID.osm http://overpass-api.de/api/map?bbox=$GPS_FROM1,$GPS_FROM2,$GPS_TO1,$GPS_TO2
+    echo "wget -N -c -O ./data/tmp/$HASH_ID.osm http://overpass-api.de/api/map?bbox=$GPS_FROM1,$GPS_FROM2,$GPS_TO1,$GPS_TO2"
+    wget -N -c -O ./data/tmp/$HASH_ID.osm http://overpass-api.de/api/map?bbox=$GPS_FROM1,$GPS_FROM2,$GPS_TO1,$GPS_TO2
   fi
   #return
 }
@@ -119,34 +118,34 @@ osm2obj()
   echo "============================================================"
   echo "OSM TO OBJECT"
   echo "============================================================"
-  if [ -a "./data/$HASH_ID.obj" ]
+  if [ -a "./data/tmp/$HASH_ID.obj" ]
   then
     echo "skipping, file already exists ..."
-    echo "./data/$HASH_ID.obj"
+    echo "./data/tmp/$HASH_ID.obj"
   else
 
-    echo "java -Djava.library.path=`pwd`/osm2world/lp_solve/ux64 -Xmx2G -jar `pwd`/osm2world/build/OSM2World.jar --config `pwd`/osm2world$OSM_MODE.properties -i `pwd`/data/$HASH_ID.osm -o `pwd`/data/$HASH_ID.obj"
+    echo "java -Djava.library.path=`pwd`/osm2world/lp_solve/ux64 -Xmx2G -jar `pwd`/osm2world/build/OSM2World.jar --config `pwd`data/osm2world/$OSM_MODE.properties -i `pwd`/data/$HASH_ID.osm -o `pwd`/data/$HASH_ID.obj"
 
     MISSING=($(java -Djava.library.path=`pwd`/osm2world/lp_solve/ux64 \
                -Xmx2G -jar `pwd`/osm2world/build/OSM2World.jar \
-               --config `pwd`/osm2world$OSM_MODE.properties \
-               -i `pwd`/data/$HASH_ID.osm \
-               -o `pwd`/data/$HASH_ID.obj \
+               --config `pwd`data/osm2world/$OSM_MODE.properties \
+               -i `pwd`/data/tmp/$HASH_ID.osm \
+               -o `pwd`/data/tmp/$HASH_ID.obj \
                2>&1 | grep "warning: missing SRTM tile" | grep -Po "[A-Z,0-9]+\.hgt"))
 
     if [[ $MISSING ]]
     then
         for hgt in "${MISSING[@]}"
         do
-            wget -N -c -P data "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Africa/$hgt.zip"
-            wget -N -c -P data "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Australia/$hgt.zip"
-            wget -N -c -P data "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/$hgt.zip"
-            wget -N -c -P data "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Islands/$hgt.zip"
-            wget -N -c -P data "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/North_America/$hgt.zip"
-            wget -N -c -P data "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/South_America/$hgt.zip"
+            wget -N -c -P data/tmp "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Africa/$hgt.zip"
+            wget -N -c -P data/tmp "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Australia/$hgt.zip"
+            wget -N -c -P data/tmp "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/$hgt.zip"
+            wget -N -c -P data/tmp "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Islands/$hgt.zip"
+            wget -N -c -P data/tmp "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/North_America/$hgt.zip"
+            wget -N -c -P data/tmp "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/South_America/$hgt.zip"
         done
 
-        cd data
+        cd data/tmp
         unzip '*.zip'
         rm *.zip
         cd ..
@@ -154,10 +153,10 @@ osm2obj()
         osm2obj
     fi
 
-    echo "meshlabserver -i ./data/$HASH_ID.obj -o ./data/clean.obj"
-    meshlabserver -i ./data/$HASH_ID.obj -o ./data/clean.obj
-    echo "mv ./data/clean.obj ./data/$HASH_ID.obj"
-    mv ./data/clean.obj ./data/$HASH_ID.obj
+    echo "meshlabserver -i ./data/tmp/$HASH_ID.obj -o ./data/tmp/clean.obj"
+    meshlabserver -i ./data/tmp/$HASH_ID.obj -o ./data/tmp/clean.obj
+    echo "mv ./data/tmp/clean.obj ./data/tmp/$HASH_ID.obj"
+    mv ./data/tmp/clean.obj ./data/tmp/$HASH_ID.obj
   fi
 }
 
@@ -173,42 +172,42 @@ grab_meta()
   echo "============================================================"
   echo "GRABBING SATTELITE IMAGE FROM BING"
   echo "============================================================"
-  if [ -a "./data/$HASH_ID.jpeg" ]
+  if [ -a "./data/tmp/$HASH_ID.jpeg" ]
   then
     echo "skipping, file already exists ..."
-    echo "./data/$HASH_ID.jpeg"
+    echo "./data/tmp/$HASH_ID.jpeg"
   else
     #echo "wget -N -c -O ./data/$HASH_ID.jpeg  http://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial?mapArea=$2,$1,$4,$3\&format=$FMT\&ms=$WIDTH,$HEIGHT\&key=$KEY"
-    wget -N -c -O ./data/$HASH_ID.$IMG_FORMAT  http://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial?mapArea=$GPS_FROM2,$GPS_FROM1,$GPS_TO2,$GPS_TO1\&format=$IMG_FORMAT\&ms=$IMG_WIDTH,$IMG_HEIGHT\&key=$KEY
+    wget -N -c -O ./data/tmp/$HASH_ID.$IMG_FORMAT  http://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial?mapArea=$GPS_FROM2,$GPS_FROM1,$GPS_TO2,$GPS_TO1\&format=$IMG_FORMAT\&ms=$IMG_WIDTH,$IMG_HEIGHT\&key=$KEY
 
     echo "============================================================"
     echo "GRABBING META DATA FROM BING"
     echo "============================================================"
     #echo "wget -N -c -O ./data/$HASH_ID.x  http://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial?mapArea=$2,$1,$4,$3\&format=$FMT\&ms=$WIDTH,$HEIGHT\&key=$KEY"
-    wget -N -c -O ./data/$HASH_ID.x  http://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial?mapArea=$GPS_FROM2,$GPS_FROM1,$GPS_TO2,$GPS_TO1\&format=$IMG_FORMAT\&ms=$IMG_WIDTH,$IMG_HEIGHT\&key=$KEY\&mmd=1
+    wget -N -c -O ./data/tmp/$HASH_ID.x  http://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial?mapArea=$GPS_FROM2,$GPS_FROM1,$GPS_TO2,$GPS_TO1\&format=$IMG_FORMAT\&ms=$IMG_WIDTH,$IMG_HEIGHT\&key=$KEY\&mmd=1
 
     SEGMENTS=20
     #echo "wget -N -c -O ./data/$HASH_ID.xx http://dev.virtualearth.net/REST/v1/Elevation/Bounds?bounds=$2,$1,$4,$3\&rows=$SEGMENTS\&cols=$SEGMENTS\&key=$KEY"
-    wget -N -c -O ./data/$HASH_ID.xx http://dev.virtualearth.net/REST/v1/Elevation/Bounds?bounds=$GPS_FROM2,$GPS_FROM1,$GPS_TO2,$GPS_TO1\&rows=$SEGMENTS\&cols=$SEGMENTS\&key=$KEY
+    wget -N -c -O ./data/tmp/$HASH_ID.xx http://dev.virtualearth.net/REST/v1/Elevation/Bounds?bounds=$GPS_FROM2,$GPS_FROM1,$GPS_TO2,$GPS_TO1\&rows=$SEGMENTS\&cols=$SEGMENTS\&key=$KEY
 
-    CENTER=(`cat data/$HASH_ID.x | grep -Po "mapCenter\"\:\{[^\}]+" | grep -Po "\[[^\]]+" | grep -Po "\-?[0-9]+\.[0-9]+"`)
-    ALTITUDE=(`cat data/$HASH_ID.xx | grep -Po "elevations\"\:\[[^\]]+" | grep -Po "[0-9]+" | sort | head -1`)
-    ZOOM=(`cat data/$HASH_ID.x | grep -Po "zoom\"\:\"[^\"]+" | grep -Po "[0-9]+"`)
-    HEIGHT=(`cat data/$HASH_ID.x | grep -Po "imageHeight\"\:\"[^\"]+" | grep -Po "[0-9]+"`)
-    WIDTH=(`cat data/$HASH_ID.x | grep -Po "imageWidth\"\:\"[^\"]+" | grep -Po "[0-9]+"`)
+    CENTER=(`cat data/tmp/$HASH_ID.x | grep -Po "mapCenter\"\:\{[^\}]+" | grep -Po "\[[^\]]+" | grep -Po "\-?[0-9]+\.[0-9]+"`)
+    ALTITUDE=(`cat data/tmp/$HASH_ID.xx | grep -Po "elevations\"\:\[[^\]]+" | grep -Po "[0-9]+" | sort | head -1`)
+    ZOOM=(`cat data/tmp/$HASH_ID.x | grep -Po "zoom\"\:\"[^\"]+" | grep -Po "[0-9]+"`)
+    HEIGHT=(`cat data/tmp/$HASH_ID.x | grep -Po "imageHeight\"\:\"[^\"]+" | grep -Po "[0-9]+"`)
+    WIDTH=(`cat data/tmp/$HASH_ID.x | grep -Po "imageWidth\"\:\"[^\"]+" | grep -Po "[0-9]+"`)
 
     SCALE=`python -c "import math; print 156543.04 * abs(math.cos(${CENTER[0]} * math.pi / 180)) / (2 ** $ZOOM)"`
 
-    echo "#QUAD: $GPS_FROM1,$GPS_FROM2,$GPS_TO1,$GPS_TO2" >> data/$HASH_ID.meta
-    echo "#IMG_FORMAT: $IMG_FORMAT" >> data/$HASH_ID.meta
-    echo "#IMG_HEIGHT: $HEIGHT"     >> data/$HASH_ID.meta
-    echo "#IMG_WIDTH: $WIDTH"       >> data/$HASH_ID.meta
-    echo "DATE: `date`"             >> data/$HASH_ID.meta
-    echo "CENTER: ${CENTER[0]} ${CENTER[1]} $ALTITUDE" > data/$HASH_ID.meta
-    echo "ZOOM: $ZOOM"              >> data/$HASH_ID.meta
-    echo "SCALE: $SCALE"            >> data/$HASH_ID.meta
+    echo "#QUAD: $GPS_FROM1,$GPS_FROM2,$GPS_TO1,$GPS_TO2" >> data/tmp/$HASH_ID.meta
+    echo "#IMG_FORMAT: $IMG_FORMAT" >> data/tmp/$HASH_ID.meta
+    echo "#IMG_HEIGHT: $HEIGHT"     >> data/tmp/$HASH_ID.meta
+    echo "#IMG_WIDTH: $WIDTH"       >> data/tmp/$HASH_ID.meta
+    echo "DATE: `date`"             >> data/tmp/$HASH_ID.meta
+    echo "CENTER: ${CENTER[0]} ${CENTER[1]} $ALTITUDE" > data/tmp/$HASH_ID.meta
+    echo "ZOOM: $ZOOM"              >> data/tmp/$HASH_ID.meta
+    echo "SCALE: $SCALE"            >> data/tmp/$HASH_ID.meta
 
-    rm data/$HASH_ID.x*
+    rm data/tmp/$HASH_ID.x*
   fi
 }
 
@@ -217,8 +216,8 @@ analyse()
   for i in $(seq 1 1 $NUMBER_OF_PROCESSES)
   do
     python ode/main.py \
-        --file  `pwd`/data/$HASH_ID.obj \
-        --image `pwd`/data/$HASH_ID.$IMG_FORMAT \
+        --file  `pwd`/data/tmp/$HASH_ID.obj \
+        --image `pwd`/data/tmp/$HASH_ID.$IMG_FORMAT \
         --image_params $IMG_WIDTH $IMG_HEIGHT $SCALE \
         --ops $OPS \
         --time `expr $SCAN_TIME_START + $i \* $SCAN_TIME_INC` $SCAN_TIME_STOP `expr $SCAN_TIME_INC \* $NUMBER_OF_PROCESSES` \
@@ -235,8 +234,8 @@ analyse()
 analyse-interactive()
 {
   python ode/main.py \
-    --file  `pwd`/data/$HASH_ID.obj \
-    --image `pwd`/data/$HASH_ID.$IMG_FORMAT \
+    --file  `pwd`/data/tmp/$HASH_ID.obj \
+    --image `pwd`/data/tmp/$HASH_ID.$IMG_FORMAT \
     --image_params $IMG_WIDTH $IMG_HEIGHT $SCALE \
     --ops $OPS \
     --time $SCAN_TIME_START $SCAN_TIME_STOP $SCAN_TIME_INC \
@@ -277,21 +276,19 @@ case "$1" in
     osm2obj
   ;;
   "--clean-all")
-    cd data
+    cd data/tmp
     ls * | xargs rm -rf
     cd ..
   ;;
   "--clean")
     parse_config $2
-    rm data/$HASH_ID*
+    rm data/tmp/$HASH_ID*
     rm -rf $OUTPUT_FOLDER
   ;;
   "--make")
     cd osm2world
     ant jar
     cd ..
-    mkdir data
-    mkdir data/gps-ops
     grab_norad
     cd odeViz
     sudo python setup.py install
